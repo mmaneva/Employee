cecf8f3d6a878838f98b617a5f8c2425e995c331
Margarita Maneva <margarita.maneva@outlook.com>
2021-11-02 09:15:28 +0100

    Add project files.

---
+++ Employee/Models/Employeecs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.Models
+{
+    public class Employeecs
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float Salary { get; set; }
+    }
+}
